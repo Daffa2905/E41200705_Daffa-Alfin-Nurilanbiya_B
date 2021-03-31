7f8a81a3d96a799f850f2ee9c14e5ad6654d48f7
Daffa Alfin Nurilanbiya <75287752+Daffa2905@users.noreply.github.com>
2021-03-31 07:40:54 -0800

    Add files via upload

---
+++ WSIBD_Latihan/Week 4/W04_Latihan_2/W04_Latihan_2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package W04_Latihan_2;
+
+/**
+ *
+ * @author DAFFA ALFIN NURILANBIYA-E41200705
+ */
+public class W04_Latihan_2 {
+
+   public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        } while (i <= 10);
+    }
+}
